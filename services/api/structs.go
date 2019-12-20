b6acdbce048d38871ec72a7c0a6e13f1614286a2
muyun001 <1219258598@qq.com>
2019-12-20 09:14:10 +0800

    header中添加字段

--- services/api/structs.go
+++ services/api/structs.go
@@ -6,6 +6,16 @@ type SendRequest struct {
 }
 
 type Headers struct {
-	UserAgent string `json:"user_agent"`
-	Cookie    string `json:"cookie"`
+	Accept                  string `json:"accept"`
+	AcceptEncoding          string `json:"accept_encoding"`
+	AcceptLanguage          string `json:"accept_language"`
+	CacheControl            string `json:"cache_control"`
+	Connection              string `json:"connection"`
+	Cookie                  string `json:"cookie"`
+	Host                    string `json:"host"`
+	SecFetchMode            string `json:"sec_fetch_mode"`
+	SecFetchSite            string `json:"sec_fetch_site"`
+	SecFetchUser            string `json:"sec_fetch_user"`
+	UpgradeInsecureRequests string `json:"upgrade_insecure_requests"`
+	UserAgent               string `json:"user_agent"`
 }
